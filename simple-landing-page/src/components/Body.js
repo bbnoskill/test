bb93f5a8c24e7385ef93497c59c78fbe7fbb7471
bbnoskill <vladandriievskiy@gmail.com>
2024-11-19 17:41:21 +0200

    Update Body.js

--- simple-landing-page/src/components/Body.js
+++ simple-landing-page/src/components/Body.js
@@ -3,42 +3,7 @@ import React from "react";
 const Body = () => {
   return (
     <main style={styles.body}>
-      <div style={styles.card}>
-        <img src={"/vitalij.jpg"} alt="Vitalij Bahurskij" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Vitalij Bahurskij</h2>
-          <p style={styles.position}>Developer</p>
-        </div>
-      </div>
-      <div style={styles.card}>
-        <img src={"/photo_2024-11-19_15-44-43.jpg"} alt="Andriievskyi Vladyslav" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Andriievskyi Vladyslav</h2>
-          <p style={styles.position}>Scrum Master</p>
-        </div>
-  
-      </div>
-  <div style={styles.card}>
-        <img src={"/Evelina.png"} alt="Bashynska Evelina" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Bashynska Evelina</h2>
-          <p style={styles.position}>Backend engineer</p>
-        </div>
- </div>
-  <div style={styles.card}>
-        <img src={"/Avatar.png"} alt="Andreyko Max" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Andreyko Max</h2>
-          <p style={styles.position}>Designer</p>
-        </div>
-    </div>
-  <div style={styles.card}>
-        <img src={"/photo_2024-11-19_15-14-57.jpg"} alt="Holub Yulia" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Holub Yulia</h2>
-          <p style={styles.position}>QA</p>
-        </div>
-   </div>
+     
     </main>
   );
 };
